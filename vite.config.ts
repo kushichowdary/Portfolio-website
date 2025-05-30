9c1418dbebf1105b95bd1335664d501afa1074ba
kushichowdary <2200039162@kluniversity.in>
2025-05-31 00:35:12 +0530

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: Processor.env.VITE_BASE_PATH || "/Portfolio-website"
+  optimizeDeps: {
+    include: ["gsap", "split-type"],
+  },
 });
